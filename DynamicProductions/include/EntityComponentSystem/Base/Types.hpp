baeadaa228929ad69f1df7da128033191cdb60f9
Anthony Kuregian <AnthonyKuregian@yahoo.com>
2022-11-12 19:14:12 -0800

    Entity Component System Types.hpp Added

--- DynamicProductions/include/EntityComponentSystem/Base/Types.hpp
+++ DynamicProductions/include/EntityComponentSystem/Base/Types.hpp
@@ -1,1 +1,47 @@
 #pragma once
+#include <set>
+
+namespace ECS {
+
+	class System;
+	class Component;
+
+	// constants
+	const size_t MAX_ENTITY_COUNT = 5000;
+	const size_t MAX_COMPONENT_COUNT = 32;
+
+	// custom types
+	typedef size_t EntityID;
+	typedef size_t SystemTypeID;
+	typedef size_t ComponentTypeID;
+	typedef std::set<ComponentTypeID> EntitySignature;
+
+	// return a component runtime typeID
+	inline static const ComponentTypeID GetRuntimeComponentTypeID() {
+		static ComponentTypeID typeID = 0u;
+		return typeID++;
+	}
+
+	// return a system runtime typeID
+	inline static const SystemTypeID GetRuntimeSystemTypeID() {
+		static SystemTypeID typeID = 0u;
+		return typeID++;
+	}
+
+	// attach type id to component class and return it
+	template<typename T>
+	inline static const ComponentTypeID ComponentType() noexcept {
+		static_assert((std::is_base_of<Component, T>::value && !std::is_same<Component, T>::value), "INVALID TEMPLATE TYPE");
+		static const ComponentTypeID typeID = GetRuntimeComponentTypeID();
+		return typeID;
+	}
+
+	// attach type id to system class and return it
+	template<typename T>
+	inline static const SystemTypeID SystemType() noexcept {
+		static_assert((std::is_base_of<System, T>::value && !std::is_same<System, T>::value), "INVALID TEMPLATE TYPE");
+		static const SystemTypeID typeID = GetRuntimeSystemTypeID();
+		return typeID;
+	}
+
+}
--- DynamicProductions/include/IEngine/main.cpp
+++ DynamicProductions/include/IEngine/main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 #include "Core.hpp"
+#include <Types.hpp>
+
+namespace ECS {
+	struct Component {
+		virtual ~Component() {};
+	};
+}
+
+class Test1 : public ECS::Component {
+
+};
+
+class Test2 : public ECS::Component {
+
+};
+
 
 int main(void)
 {
+
+	auto typeID1 = ECS::ComponentType<Test1>();
+	auto typeID2 = ECS::ComponentType<Test1>();
+
+	auto typeID3 = ECS::ComponentType<Test2>();
+
+	std::cout << "Component: " << typeID1 << std::endl;
+	std::cout << "Component: " << typeID2 << std::endl;
+	std::cout << "Component: " << typeID3 << std::endl;
+
+
 	Dynamic::Core->Initialize();
 	Dynamic::Clock->Initialize();
 	Dynamic::Event->Initialize();
